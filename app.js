e896be9d9d2176498bb12ea2a8b2822c6a098660
Sagar Heera <sagarh@Corp.Optym.net>
2023-02-09 10:30:20 +0530

    message changed again!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is Sagar,trying AWS Devops!'));
+app.get('/', (req, res) => res.send('This is Sagar,trying AWS Devops! this text is changed for Manual Testing'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
